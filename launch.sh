c4466bdab8b14eff2d5303654d711d166e0c5534
Astrid Allemandou <aalleman@z1r1p5.le-101.fr>
2021-03-20 15:34:06 +0100

    finish html course, take screenshot on webcam

--- launch.sh
+++ launch.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker build -t camagru_image .
 docker run -v /Users/aalleman/camagru/site:/usr/share/webapps/site -d --name camagru_container -p 80:80 -p 443:443 camagru_image
-docker exec -ti camagru_container /bin/sh
