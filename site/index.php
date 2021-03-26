94ee6b63d840c07478eb775b7e95424db2b8609c
Tripouille <jmichel.gambard@gmail.com>
2021-03-26 14:12:19 +0100

    login done

--- site/index.php
+++ site/index.php
@@ -2,10 +2,22 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<link href="css/site.css" rel="stylesheet">
 		<title>Camagru</title>
 	</head>
 
 	<body>
-		coucou
+		<article id="login">
+			<h1 id="logo">Camagru</h1>
+			<form>
+				<input type="text" name="id" placeholder="Login"/>
+				<input type="password" name="password"  placeholder="Password"/>
+				<input type="submit" value="Connect"/>
+			</form>
+		</article>
+
+		<footer>
+
+		</footer>
 	</body>
 </html>
